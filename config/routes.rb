baa01ed7465348c20832431e0c11ecd40dbd6d96
Valentino Espinoza <xvalentino13@gmail.com>
2015-04-23 10:07:04 -0600

    made callback route match the twitter callback route
    added a slash

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider/callback/', to: 'sessions#create'
   root "dashboard#index"
   get '/map', to: "map#show"
   get '/account', to: "dashboard#show"
