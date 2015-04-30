74a316514ad80ef1393da0dcab59800d9710de9c
Valentino Espinoza <xvalentino13@gmail.com>
2015-04-29 23:45:54 -0600

    Map is dynamically colored based on its population

--- app/assets/javascripts/mapbox.js
+++ app/assets/javascripts/mapbox.js
@@ -1,19 +1,36 @@
 $(function() {
-  var updateStates = function(states) {
-    var colors = ['green', 'white', 'red', 'purple', 'cyan']
+
+  var sortedPopulation = null;
+
+  var correctColor = function(population) {
+
+    var max = sortedPopulation[sortedPopulation.length-1];
+    return Math.floor((population/max)*255)
+  }
+
+  var updateStates = function(states, populations) {
+    if(!sortedPopulation) {
+      sortedPopulation = Object.keys(populations).map(function(state){
+        return populations[state]
+      }).sort();
+    }
     return L.geoJson(states, {
       style: function(feature) {
         //update the style however you want
-        feature.properties.fill = colors[Math.floor(Math.random() * colors.length - 1)];
+        var green = correctColor(populations[feature.properties.NAME]);
+        console.log(green);
+        feature.properties.fill = 'rgb(0, ' + green+ ', 0 )';
       }
     }).toGeoJSON();
   }
 
   $.getJSON('/state_polygons.json').then(function(states) {
-    states = updateStates(states);
-    L.mapbox.accessToken = 'pk.eyJ1IjoieHZhbGVudGlubyIsImEiOiJ5dG43MDFjIn0.tEQIYibYnwUQNhCg1v0jtw';
-    var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([39, -98], 4);
-    var featureLayer = L.mapbox.featureLayer(states).addTo(map);
+    $.getJSON('/state_population.json').then(function(population) {
+      states = updateStates(states, population);
+      L.mapbox.accessToken = 'pk.eyJ1IjoieHZhbGVudGlubyIsImEiOiJ5dG43MDFjIn0.tEQIYibYnwUQNhCg1v0jtw';
+      var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([39, -98], 4);
+      var featureLayer = L.mapbox.featureLayer(states).addTo(map);
+    })
   })
 });
 
